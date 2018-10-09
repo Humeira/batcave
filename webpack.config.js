8ba9817a134babf17288f2fa04e7b2d5ad8ce98c
Joonas Tiala <joonas.tiala@codemate.com>
2018-10-09 14:25:16 +0300

    Update all dependecies to latest major

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,36 @@ const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    context: path.join(__dirname, 'src'),
-    entry: [
-        './app.js',
+  context: path.join(__dirname, 'src'),
+  entry: [
+      './app.js',
     ],
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'dist.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader',
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            }
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'dist.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+            'babel-loader',
         ],
-    },
-    resolve: {
-        modules: [
-            path.join(__dirname, 'node_modules'),
-        ],
-    },
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ],
+  },
+  resolve: {
+    modules: [
+        path.join(__dirname, 'node_modules'),
+    ],
+  },
 
-    plugins: [
-        new Dotenv()
+  plugins: [
+      new Dotenv()
     ]
 };
